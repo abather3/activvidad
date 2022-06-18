d481b0db880bca34e954be8b8975579514082053
QlintYahu <48577521+abather3@users.noreply.github.com>
2022-06-18 10:45:15 +0800

    Add files via upload

---
+++ WS101_DeGuzmanKlinton_Act1Mod8_JavaJS.js
@@ -0,0 +1,6 @@
+// A function to display a message 
+	function sayJava() {     
+	alert("I'm JavaScript!");
+	}
+// Call function on click of the button 
+document.getElementById("myBtn").onclick = sayJava;    
